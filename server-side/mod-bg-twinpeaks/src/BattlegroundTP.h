d927684baefd0d04866545280197cc3ce33905a0
Stefano Borzì <stefanoborzi32@gmail.com>
2026-09-21 08:33:27 +0200

    fix: build against the latest AzerothCore (#9)

--- server-side/mod-bg-twinpeaks/src/BattlegroundTP.h
+++ server-side/mod-bg-twinpeaks/src/BattlegroundTP.h
@@ -235,7 +235,7 @@ class AC_GAME_API BattlegroundTP : public Battleground
         bool UpdatePlayerScore(Player* player, uint32 type, uint32 value, bool doAddHonor = true) override;
         void SetDroppedFlagGUID(ObjectGuid guid, TeamId teamId) override { _droppedFlagGUID[teamId] = guid; }
         ObjectGuid GetDroppedFlagGUID(TeamId teamId) const { return _droppedFlagGUID[teamId];}
-        void FillInitialWorldStates(WorldPacket& data) override;
+        void FillInitialWorldStates(WorldPackets::WorldState::InitWorldStates& packet) override;
 
         /* Scorekeeping */
         void AddPoints(TeamId teamId, uint32 points) { m_TeamScores[teamId] += points; }
